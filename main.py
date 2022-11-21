57fa5194e65c66e5167ecd9c7a3fbf11ef000748
RedNoyz <111233665+RedNoyz@users.noreply.github.com>
2022-11-22 00:37:50 +0200

    Added SMS alert and pythonanywhere compatibility

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import requests
 import datetime
+import os
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
 
 # "lat": 44.426765,
 # "lon": 26.102537,
+
+proxy_client = TwilioHttpClient()
+proxy_client.session.proxies = {"https": os.environ["https_proxy"]}
+
+account_sid = "ACaad8e1c2efa15333e862b5ce18a9a076"
+auth_token = "deadb31133fc9825dac5ab444b4844a7"
+
 
 rain_codes = [1063, 1072, 1150, 1153, 1168, 1171, 1180, 1183, 1186, 1189, 1192, 1195, 1198, 1201, 1240, 1243, 1246, 1273, 1276]
 
@@ -13,7 +23,6 @@ parameters = {
 }
 
 current_hour = int(str(datetime.datetime.now()).split(" ")[1].split(":")[0])
-print(current_hour)
 
 response_weather = requests.get("http://api.weatherapi.com/v1/forecast.json", params=parameters)
 response_weather = requests.get("http://api.weatherapi.com/v1/forecast.json?q=Bucharest&days=1&key=69df0465b29a49b796712442222111")
@@ -37,9 +46,32 @@ for hour in forecast_data:
 
 item = 0
 
-print(forecast_list)
+client = Client(account_sid, auth_token, http_client=proxy_client)
+
+will_rain = False
+
+rain_hours = []
+
 
 for entry in forecast_list:
     if current_hour <= forecast_list[item]["time"] <= (current_hour + 12):
-        print(f"It will rain in the next 12 hours at {forecast_list[item]['time']}, {forecast_list[item]['condition']}, {forecast_list[item]['temp']}")
+        rain_times = {
+            "time": forecast_list[item]['time'],
+            "condition": forecast_list[item]['condition'],
+            "temp": forecast_list[item]['temp'],
+        }
+        rain_hours.append(rain_times)
+
+        will_rain = True
     item += 1
+
+start_rain = rain_hours[0]["time"]
+start_condition = rain_hours[0]["condition"]
+start_temp = rain_hours[0]["temp"]
+
+if will_rain is False:
+    message = client.messages.create(
+        body=f'It will rain in the next 12 hours, starting with {start_rain}, {start_condition} and there will be {start_temp}C',
+        from_='+13466372476',
+        to='+40720609861'
+    )
